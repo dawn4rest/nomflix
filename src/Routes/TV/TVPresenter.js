53e7323b619d398bd6a7e0471aed11f27fcff0aa
dawn4rest <dawn4rest@gmail.com>
2019-09-19 13:45:23 +0900

    feat. Set Presenters

--- src/Routes/TV/TVPresenter.js
+++ src/Routes/TV/TVPresenter.js
@@ -1,1 +1,40 @@
-export default () => "TV";
+import React from "react";
+import PropTypes from "prop-types";
+import styled from "styled-components";
+
+import Section from "Components/Section";
+
+const Container = styled.div`
+  padding: 0px 10px;
+`;
+
+const TVPresenter = ({ topRated, airingToday, popular, error, loading }) =>
+  loading ? null : (
+    <Container>
+      {topRated && topRated.length > 0 && (
+        <Section title="Top Rated TV Shows">
+          {topRated.map(show => show.name)}
+        </Section>
+      )}
+      {airingToday && airingToday.length > 0 && (
+        <Section title="Airing Today TV Shows">
+          {airingToday.map(show => show.name)}
+        </Section>
+      )}
+      {popular && popular.length > 0 && (
+        <Section title="Popular TV Shows">
+          {popular.map(show => show.name)}
+        </Section>
+      )}
+    </Container>
+  );
+
+TVPresenter.propTypes = {
+  topRated: PropTypes.array,
+  airingToday: PropTypes.array,
+  popular: PropTypes.array,
+  error: PropTypes.string,
+  loading: PropTypes.bool.isRequired
+};
+
+export default TVPresenter;
